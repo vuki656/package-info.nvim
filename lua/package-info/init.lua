23f07d71728ebf3c335f342648a706d47704312b
domagoj.vukovic <off.vukovic@gmail.com>
2021-08-02 11:28:24 +0200

    feat: detect package.json closes #4

--- lua/package-info/init.lua
+++ lua/package-info/init.lua
@@ -1,1 +1,13 @@
-print('Hello')
+function check_if_file_package_json(path)
+    return string.match(path, "package.json$")
+end
+
+local CURRENT_BUFFER = 0
+
+local current_file_path = vim.api.nvim_buf_get_name(CURRENT_BUFFER)
+
+local is_file_package_json = check_if_file_package_json(current_file_path)
+
+if is_file_package_json then
+    print("Im a package.json")
+end
