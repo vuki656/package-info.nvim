ebc35a7894f4801b695a798db72a1cefe62e83dd
domagoj.vukovic <off.vukovic@gmail.com>
2021-09-06 23:36:33 +0200

    ref: rewrite buffer parsing logic

--- lua/package-info/utils.lua
+++ lua/package-info/utils.lua
@@ -1,3 +1,7 @@
+local json_parser = require("package-info.libs.json_parser")
+
+local logger = require("package-info.logger")
+
 M = {}
 
 --- Checks if given string contains "error"
@@ -11,6 +15,89 @@ M.has_errors = function(value)
     end
 
     return string.find(string_value, "error") ~= nil
+end
+
+--- Manages loading animation state
+M.loading = {
+    animation = {
+        "⠋",
+        "⠙",
+        "⠹",
+        "⠸",
+        "⠼",
+        "⠴",
+        "⠦",
+        "⠧",
+        "⠇",
+        "⠏",
+    },
+    index = 1,
+    log = "",
+    spinner = "",
+    is_running = false,
+    fetch = function()
+        return M.loading.spinner .. " " .. M.loading.log
+    end,
+    start = function(message)
+        M.loading.log = message
+        M.loading.is_running = true
+        M.loading.update()
+    end,
+    stop = function()
+        M.loading.is_running = false
+        M.loading.log = ""
+        M.loading.spinner = ""
+    end,
+    update = function()
+        if M.loading.is_running then
+            M.loading.spinner = M.loading.animation[M.loading.index]
+
+            M.loading.index = M.loading.index + 1
+
+            if M.loading.index == 10 then
+                M.loading.index = 1
+            end
+
+            vim.fn.timer_start(80, function()
+                M.loading.update()
+            end)
+        end
+    end,
+}
+
+
+--- Runs an async job
+-- @param options.command - string command to run
+-- @param options.json - boolean if output should be parsed as json
+-- @param options.callback - function to invoke with the results
+function job(options)
+    local value = ""
+
+    vim.fn.jobstart(options.command, {
+        on_stdout = function(_, stdout)
+            value = value .. table.concat(stdout)
+
+            if table.concat(stdout) == "" then
+                local has_error = M.has_errors(stdout)
+
+                if has_error then
+                    logger.error("Error running " .. options.command .. ". Try running manually.")
+
+                    return
+                end
+
+                if options.json then
+                    local json_value = json_parser.decode(value)
+
+                    options.callback(json_value)
+
+                    return
+                end
+
+                options.callback(value)
+            end
+        end,
+    })
 end
 
 return M
