6771b0f1c600ba0ef0d80d140f5c8d32a8698b02
domagoj.vukovic <off.vukovic@gmail.com>
2021-09-13 17:26:06 +0200

    ref(tests): dont use shorthand variables

--- lua/package-info/tests/utils.spec.lua
+++ lua/package-info/tests/utils.spec.lua
@@ -2,46 +2,43 @@ local utils = require("package-info.utils")
 local constants = require("package-info.constants")
 local config = require("package-info.config")
 
-equals = assert.are.equals
-not_equals = assert.are_not.equals
-
 describe("Command retrieval for yarn", function()
     config.options.package_manager = "yarn"
 
     it("should get the delete command", function()
         local command = utils.get_command.delete("prettier")
 
-        equals(command, "yarn remove prettier")
+        assert.are.equals(command, "yarn remove prettier")
     end)
 
     it("should get the update command", function()
         local command = utils.get_command.update("prettier")
 
-        equals(command, "yarn upgrade --latest prettier")
+        assert.are.equals(command, "yarn upgrade --latest prettier")
     end)
 
     it("should get the development install command", function()
         local command = utils.get_command.install(constants.DEPENDENCY_TYPE.development, "prettier")
 
-        equals(command, "yarn add -D prettier")
+        assert.are.equals(command, "yarn add -D prettier")
     end)
 
     it("should get the production install command", function()
         local command = utils.get_command.install(constants.DEPENDENCY_TYPE.production, "prettier")
 
-        equals(command, "yarn add prettier")
+        assert.are.equals(command, "yarn add prettier")
     end)
 
     it("should get the reinstall command", function()
         local command = utils.get_command.reinstall()
 
-        equals(command, "rm -rf node_modules && yarn")
+        assert.are.equals(command, "rm -rf node_modules && yarn")
     end)
 
     it("should get the change version command", function()
         local command = utils.get_command.change_version("prettier", "3.0.0")
 
-        equals(command, "yarn upgrade prettier@3.0.0")
+        assert.are.equals(command, "yarn upgrade prettier@3.0.0")
     end)
 end)
 
@@ -51,37 +48,37 @@ describe("Command retrieval for npm", function()
     it("should get the delete command", function()
         local command = utils.get_command.delete("prettier")
 
-        equals(command, "npm uninstall prettier")
+        assert.are.equals(command, "npm uninstall prettier")
     end)
 
     it("should get the update command", function()
         local command = utils.get_command.update("prettier")
 
-        equals(command, "npm install prettier@latest")
+        assert.are.equals(command, "npm install prettier@latest")
     end)
 
     it("should get the development install command", function()
         local command = utils.get_command.install(constants.DEPENDENCY_TYPE.development, "prettier")
 
-        equals(command, "npm install --save-dev prettier")
+        assert.are.equals(command, "npm install --save-dev prettier")
     end)
 
     it("should get the production install command", function()
         local command = utils.get_command.install(constants.DEPENDENCY_TYPE.production, "prettier")
 
-        equals(command, "npm install prettier")
+        assert.are.equals(command, "npm install prettier")
     end)
 
     it("should get the reinstall command", function()
         local command = utils.get_command.reinstall()
 
-        equals(command, "rm -rf node_modules && npm install")
+        assert.are.equals(command, "rm -rf node_modules && npm install")
     end)
 
     it("should get the change version command", function()
         local command = utils.get_command.change_version("prettier", "3.0.0")
 
-        equals(command, "npm install prettier@3.0.0")
+        assert.are.equals(command, "npm install prettier@3.0.0")
     end)
 end)
 
@@ -89,13 +86,13 @@ describe("Command retrieval", function()
     it("should get the version list command", function()
         local command = utils.get_command.version_list("prettier")
 
-        equals(command, "npm view prettier versions --json")
+        assert.are.equals(command, "npm view prettier versions --json")
     end)
 
     it("should get the outdated packages command", function()
         local command = utils.get_command.outdated()
 
-        equals(command, "npm outdated --json")
+        assert.are.equals(command, "npm outdated --json")
     end)
 end)
 
@@ -103,7 +100,7 @@ describe("Loading", function()
     it("should start", function()
         utils.loading.start("Installing prettier")
 
-        equals(utils.loading.is_running, true)
+        assert.are.equals(utils.loading.is_running, true)
     end)
 
     it("should start", function()
@@ -111,8 +108,8 @@ describe("Loading", function()
         utils.loading.stop()
 
         assert.are.equals(utils.loading.is_running, false)
-        equals(utils.loading.log, "")
-        equals(utils.loading.spinner, "")
+        assert.are.equals(utils.loading.log, "")
+        assert.are.equals(utils.loading.spinner, "")
     end)
 
     it("should set the message", function()
@@ -120,13 +117,13 @@ describe("Loading", function()
 
         utils.loading.start(message)
 
-        equals(utils.loading.log, message)
+        assert.are.equals(utils.loading.log, message)
     end)
 
     it("should start the loading animation", function()
         utils.loading.start("Installing prettier")
 
-        not_equals(utils.loading.spinner, "")
+        assert.are_not.equals(utils.loading.spinner, "")
     end)
 
     it("should get the status", function()
