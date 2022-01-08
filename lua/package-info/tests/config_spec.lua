42af6ad13be421b8e82eb1a08fb6794e21264549
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-08 23:29:41 +0100

    ref: group config tests based on function

--- lua/package-info/tests/config_spec.lua
+++ lua/package-info/tests/config_spec.lua
@@ -5,169 +5,186 @@ local state = require("package-info.state")
 local file = require("package-info.tests.utils.file")
 
 describe("Config", function()
-    it("should register user options properly", function()
-        local new_config = {
-            colors = {
-                up_to_date = "#ffffff",
-                outdated = "#333333",
-            },
-            icons = {
-                enable = false,
-                style = {
-                    up_to_date = "GG",
-                    outdated = "NN",
+    describe("register_user_options", function()
+        it("should register user options", function()
+            local new_config = {
+                colors = {
+                    up_to_date = "#ffffff",
+                    outdated = "#333333",
                 },
-            },
-            autostart = false,
-            package_manager = constants.PACKAGE_MANAGERS.yarn,
-            hide_up_to_date = true,
-            hide_unstable_versions = true,
-        }
+                icons = {
+                    enable = false,
+                    style = {
+                        up_to_date = "GG",
+                        outdated = "NN",
+                    },
+                },
+                autostart = false,
+                package_manager = constants.PACKAGE_MANAGERS.yarn,
+                hide_up_to_date = true,
+                hide_unstable_versions = true,
+            }
 
-        config.__register_user_options(new_config)
+            config.__register_user_options(new_config)
 
-        assert.are.same(new_config, config.options)
-    end)
+            assert.are.same(new_config, config.options)
+        end)
 
-    it("should keep default options if not changed by the user", function()
-        local new_config = {
-            colors = {
-                up_to_date = "#ffffff",
-                outdated = "#333333",
-            },
-        }
+        it("should keep default options if not changed by the user", function()
+            local new_config = {
+                colors = {
+                    up_to_date = "#ffffff",
+                    outdated = "#333333",
+                },
+            }
 
-        config.__register_user_options(new_config)
+            config.__register_user_options(new_config)
 
-        local merged_config = vim.tbl_deep_extend("keep", config.options, new_config)
+            local merged_config = vim.tbl_deep_extend("keep", config.options, new_config)
 
-        assert.are.same(merged_config, config.options)
-    end)
-
-    it("should detect npm package manager", function()
-        local file_name = "package-lock.json"
-
-        file.create(file_name)
-
-        config.__register_package_manager()
-
-        file.delete(file_name)
-
-        assert.are.equals(constants.PACKAGE_MANAGERS.npm, config.options.package_manager)
-    end)
-
-    it("should detect yarn package manager", function()
-        local file_name = "yarn.lock"
-
-        file.create(file_name)
-
-        config.__register_package_manager()
-
-        file.delete(file_name)
-
-        assert.are.equals(constants.PACKAGE_MANAGERS.yarn, config.options.package_manager)
-    end)
-
-    it("should detect pnpm package manager", function()
-        local file_name = "pnpm-lock.yaml"
-
-        file.create(file_name)
-
-        config.__register_package_manager()
-
-        file.delete(file_name)
-
-        assert.are.equals(constants.PACKAGE_MANAGERS.pnpm, config.options.package_manager)
-    end)
-
-    it("should register namespace", function()
-        config.__register_namespace()
-
-        assert.are.equals(1, state.namespace.id)
-    end)
-
-    it("should register load plugin command", function()
-        config.__register_start()
-
-        local autocommands = vim.api.nvim_exec("autocmd BufEnter", true)
-
-        local is_registered = string.find(autocommands, "require('package-info.core').load_plugin()", 0, true)
-
-        assert.is_true(is_registered ~= nil)
-    end)
-
-    it("should register colors", function()
-        vim.cmd('let g:colors_name="weird-theme"')
-        vim.cmd("colorscheme")
-
-        config.__register_colorscheme_initialization()
-
-        local autocommands = vim.api.nvim_exec("autocmd ColorScheme", true)
-
-        local is_registered = string.find(
-            autocommands,
-            "lua require('package-info.config').__register_highlight_groups()",
-            0,
-            true
-        )
-
-        assert.is_true(is_registered ~= nil)
-    end)
-
-    it("should register colors if default theme is registered", function()
-        vim.cmd('let g:colors_name="default"')
-        vim.cmd("colorscheme")
-
-        config.__register_colorscheme_initialization()
-
-        assert.has_no.errors(function()
-            vim.cmd("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date)
-            vim.cmd("highlight " .. constants.HIGHLIGHT_GROUPS.outdated)
+            assert.are.same(merged_config, config.options)
         end)
     end)
 
-    it("should register colors if termguicolors is available", function()
-        vim.cmd("set termguicolors")
+    describe("register_package_manager", function()
+        it("should detect npm package manager", function()
+            local file_name = "package-lock.json"
 
-        config.__register_colorscheme_initialization()
+            file.create(file_name)
 
-        local up_to_date_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date, true)
-        local outdated_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.outdated, true)
+            config.__register_package_manager()
 
-        local is_up_to_date_color_registered = string.find(up_to_date_color, config.options.colors.up_to_date, 0, true)
-        local is_outdated_color_registered = string.find(outdated_color, config.options.colors.outdated, 0, true)
+            file.delete(file_name)
 
-        assert.is_true(is_outdated_color_registered ~= nil)
-        assert.is_true(is_up_to_date_color_registered ~= nil)
+            assert.are.equals(constants.PACKAGE_MANAGERS.npm, config.options.package_manager)
+        end)
+
+        it("should detect yarn package manager", function()
+            local file_name = "yarn.lock"
+
+            file.create(file_name)
+
+            config.__register_package_manager()
+
+            file.delete(file_name)
+
+            assert.are.equals(constants.PACKAGE_MANAGERS.yarn, config.options.package_manager)
+        end)
+
+        it("should detect pnpm package manager", function()
+            local file_name = "pnpm-lock.yaml"
+
+            file.create(file_name)
+
+            config.__register_package_manager()
+
+            file.delete(file_name)
+
+            assert.are.equals(constants.PACKAGE_MANAGERS.pnpm, config.options.package_manager)
+        end)
     end)
 
-    it("should register colors if termguicolors not available", function()
-        vim.cmd("set notermguicolors")
+    describe("register_namespace", function()
+        it("should register namespace", function()
+            config.__register_namespace()
 
-        config.__register_colorscheme_initialization()
-
-        local up_to_date_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date, true)
-        local outdated_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.outdated, true)
-
-        local is_up_to_date_color_registered = string.find(
-            up_to_date_color,
-            constants.LEGACY_COLORS.up_to_date,
-            0,
-            true
-        )
-        local is_outdated_color_registered = string.find(outdated_color, constants.LEGACY_COLORS.outdated, 0, true)
-
-        assert.is_true(is_outdated_color_registered ~= nil)
-        assert.is_true(is_up_to_date_color_registered ~= nil)
+            assert.are.equals(1, state.namespace.id)
+        end)
     end)
 
-    it("should register commands", function()
-        config.__register_commands()
+    describe("register_start", function()
+        it("should register load command", function()
+            config.__register_start()
 
-        assert.has_no.errors(function()
-            for _, command in pairs(constants.COMMANDS) do
-                vim.cmd(command)
-            end
+            local autocommands = vim.api.nvim_exec("autocmd BufEnter", true)
+
+            local is_registered = string.find(autocommands, "require('package-info.core').load_plugin()", 0, true)
+
+            assert.is_true(is_registered ~= nil)
+        end)
+    end)
+
+    describe("register_colorscheme_initialization", function()
+        it("should register colors", function()
+            vim.cmd('let g:colors_name="weird-theme"')
+            vim.cmd("colorscheme")
+
+            config.__register_colorscheme_initialization()
+
+            local autocommands = vim.api.nvim_exec("autocmd ColorScheme", true)
+
+            local is_registered = string.find(
+                autocommands,
+                "lua require('package-info.config').__register_highlight_groups()",
+                0,
+                true
+            )
+
+            assert.is_true(is_registered ~= nil)
+        end)
+
+        it("should register colors if default theme is registered", function()
+            vim.cmd('let g:colors_name="default"')
+            vim.cmd("colorscheme")
+
+            config.__register_colorscheme_initialization()
+
+            assert.has_no.errors(function()
+                vim.cmd("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date)
+                vim.cmd("highlight " .. constants.HIGHLIGHT_GROUPS.outdated)
+            end)
+        end)
+
+        it("should register colors if termguicolors is available", function()
+            vim.cmd("set termguicolors")
+
+            config.__register_colorscheme_initialization()
+
+            local up_to_date_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date, true)
+            local outdated_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.outdated, true)
+
+            local is_up_to_date_color_registered = string.find(
+                up_to_date_color,
+                config.options.colors.up_to_date,
+                0,
+                true
+            )
+            local is_outdated_color_registered = string.find(outdated_color, config.options.colors.outdated, 0, true)
+
+            assert.is_true(is_outdated_color_registered ~= nil)
+            assert.is_true(is_up_to_date_color_registered ~= nil)
+        end)
+
+        it("should register colors if termguicolors not available", function()
+            vim.cmd("set notermguicolors")
+
+            config.__register_colorscheme_initialization()
+
+            local up_to_date_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date, true)
+            local outdated_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.outdated, true)
+
+            local is_up_to_date_color_registered = string.find(
+                up_to_date_color,
+                constants.LEGACY_COLORS.up_to_date,
+                0,
+                true
+            )
+            local is_outdated_color_registered = string.find(outdated_color, constants.LEGACY_COLORS.outdated, 0, true)
+
+            assert.is_true(is_outdated_color_registered ~= nil)
+            assert.is_true(is_up_to_date_color_registered ~= nil)
+        end)
+    end)
+
+    describe("register_commands", function()
+        it("should register commands", function()
+            config.__register_commands()
+
+            assert.has_no.errors(function()
+                for _, command in pairs(constants.COMMANDS) do
+                    vim.cmd(command)
+                end
+            end)
         end)
     end)
 end)
