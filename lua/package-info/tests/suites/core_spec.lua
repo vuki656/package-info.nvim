f56e8371e259e3780d2dbc01f29bad7e1ac61087
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-09 17:26:46 +0100

    ref: extract is_valid_package_version test to separate file

--- lua/package-info/tests/suites/core_spec.lua
+++ lua/package-info/tests/suites/core_spec.lua
@@ -18,20 +18,6 @@ describe("Core", function()
         state.reset()
     end)
 
-    describe("is_valid_package_version", function()
-        it("should return true if version is valid", function()
-            local is_valid = core.__is_valid_package_version("1.0.0")
-
-            assert.is_true(is_valid)
-        end)
-
-        it("should return false if version is invalid", function()
-            local is_valid = core.__is_valid_package_version("test")
-
-            assert.is_false(is_valid)
-        end)
-    end)
-
     describe("decode_json_string", function()
         it("should return nil if json is invalid", function()
             local json_value = core.__decode_json_string('{ l "name": "test" }')
