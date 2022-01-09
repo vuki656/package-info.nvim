b7d70c920e0b5b89432544817bd17309d832c09d
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-09 16:13:56 +0100

    feat: core clear_virtual_text tests

--- lua/package-info/tests/suites/core_spec.lua
+++ lua/package-info/tests/suites/core_spec.lua
@@ -10,7 +10,6 @@ local state = require("package-info.state")
 local config = require("package-info.config")
 local constants = require("package-info.utils.constants")
 local logger = require("package-info.utils.logger")
-local to_boolean = require("package-info.utils.to-boolean")
 
 local file = require("package-info.tests.utils.file")
 
@@ -670,6 +669,50 @@ describe("Core", function()
                     },
                 },
             })
+
+            file.delete(file_name)
+        end)
+    end)
+
+    describe("clear_virtual_text", function()
+        it("shouldn't run if nothing is displayed", function()
+            state.displayed = false
+
+            spy.on(vim.api, "nvim_buf_clear_namespace")
+
+            core.clear_virtual_text()
+
+            assert.spy(vim.api.nvim_buf_clear_namespace).was_called(0)
+        end)
+
+        it("shouldn clear all existing virtual text", function()
+            state.displayed = false
+
+            local file_name = "package.json"
+
+            file.create(
+                file_name,
+                [[
+                {
+                    "dependencies": {
+                        "react": "16.0.0"
+                    }
+                }
+                ]]
+            )
+            file.go(file_name)
+
+            spy.on(vim.api, "nvim_buf_clear_namespace")
+
+            config.setup()
+            core.load_plugin()
+            core.display_virtual_text()
+            core.clear_virtual_text()
+
+            local virtual_text_positions = vim.api.nvim_buf_get_extmarks(state.buffer.id, state.namespace.id, 0, -1, {})
+
+            assert.spy(vim.api.nvim_buf_clear_namespace).was_called(1)
+            assert.is_true(vim.tbl_isempty(virtual_text_positions))
 
             file.delete(file_name)
         end)
