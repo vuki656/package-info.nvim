080fc21eae3a6a64146e7edbfe469a63533ea619
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-10 00:23:19 +0100

    ref: extract register_colorschem_initialization test to separate file

---
+++ lua/package-info/tests/suites/config/register_colorscheme_initialization_spec.lua
@@ -0,0 +1,73 @@
+local constants = require("package-info.utils.constants")
+local config = require("package-info.config")
+local to_boolean = require("package-info.utils.to-boolean")
+
+local reset = require("package-info.tests.utils.reset")
+
+describe("Config register_colorscheme_initialization", function()
+    before_each(function()
+        reset.config()
+    end)
+
+    it("should register colors", function()
+        vim.cmd('let g:colors_name="weird-theme"')
+        vim.cmd("colorscheme")
+
+        config.__register_colorscheme_initialization()
+
+        local autocommands = vim.api.nvim_exec("autocmd ColorScheme", true)
+
+        local is_registered = to_boolean(
+            string.find(autocommands, "lua require('package-info.config').__register_highlight_groups()", 0, true)
+        )
+
+        assert.is_true(is_registered)
+    end)
+
+    it("should register colors if default theme is registered", function()
+        vim.cmd('let g:colors_name="default"')
+        vim.cmd("colorscheme")
+
+        config.__register_colorscheme_initialization()
+
+        assert.has_no.errors(function()
+            vim.cmd("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date)
+            vim.cmd("highlight " .. constants.HIGHLIGHT_GROUPS.outdated)
+        end)
+    end)
+
+    it("should register colors if termguicolors is available", function()
+        vim.cmd("set termguicolors")
+
+        config.__register_colorscheme_initialization()
+
+        local up_to_date_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date, true)
+        local outdated_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.outdated, true)
+
+        local is_up_to_date_color_registered = string.find(up_to_date_color, config.options.colors.up_to_date, 0, true)
+        local is_outdated_color_registered = string.find(outdated_color, config.options.colors.outdated, 0, true)
+
+        assert.is_not_nil(is_outdated_color_registered)
+        assert.is_not_nil(is_up_to_date_color_registered)
+    end)
+
+    it("should register colors if termguicolors not available", function()
+        vim.cmd("set notermguicolors")
+
+        config.__register_colorscheme_initialization()
+
+        local up_to_date_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.up_to_date, true)
+        local outdated_color = vim.api.nvim_exec("highlight " .. constants.HIGHLIGHT_GROUPS.outdated, true)
+
+        local is_up_to_date_color_registered = string.find(
+            up_to_date_color,
+            constants.LEGACY_COLORS.up_to_date,
+            0,
+            true
+        )
+        local is_outdated_color_registered = string.find(outdated_color, constants.LEGACY_COLORS.outdated, 0, true)
+
+        assert.is_not_nil(is_outdated_color_registered)
+        assert.is_not_nil(is_up_to_date_color_registered)
+    end)
+end)
