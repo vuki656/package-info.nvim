a6f4539ebe1d1244831989fa53196e7d978183c9
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-10 00:32:07 +0100

    ref: extract register_commands tests to separate file

---
+++ lua/package-info/tests/suites/config/register_commands_spec.lua
@@ -0,0 +1,20 @@
+local constants = require("package-info.utils.constants")
+local config = require("package-info.config")
+
+local reset = require("package-info.tests.utils.reset")
+
+describe("Config register_commands", function()
+    before_each(function()
+        reset.config()
+    end)
+
+    it("should register commands", function()
+        config.__register_commands()
+
+        assert.has_no.errors(function()
+            for _, command in pairs(constants.COMMANDS) do
+                vim.cmd(command)
+            end
+        end)
+    end)
+end)
