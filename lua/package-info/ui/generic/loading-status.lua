cdd11c2e3d1646def38e1a7d770e026b3eed9cee
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-11 23:31:07 +0100

    fix: loading spinner freezing on multiple queued items

--- lua/package-info/ui/generic/loading-status.lua
+++ lua/package-info/ui/generic/loading-status.lua
@@ -1,5 +1,3 @@
--- FIXME: current spinner stops if there is another one queued up
-
 local SPINNERS = {
     "⠋",
     "⠙",
@@ -14,119 +12,101 @@ local SPINNERS = {
 }
 
 local M = {
-    instances = {},
+    queue = {},
+    state = {
+        current_spinner = "",
+        index = 1,
+        is_running = false,
+    },
 }
 
 --- Spawn a new loading instance
 -- @param log: string - message to display in the loading status
 -- @return number - id of the created instance
-M.new = function(log)
-    local state = {
-        index = 1,
+M.new = function(message)
+    local instance = {
         id = math.random(),
-        log = log,
-        current_spinner = "",
+        message = message,
         is_ready = false,
-        is_running = false,
     }
 
-    --- Get the current loading status message
-    -- @return nil
-    local function get_message()
-        return state.current_spinner .. " " .. state.log
-    end
-
-    --- Update the spinner instance recursively
-    -- @return nil
-    local function update()
-        state.current_spinner = SPINNERS[state.index]
-
-        state.index = state.index + 1
-
-        if state.index == 10 then
-            state.index = 1
-        end
-
-        vim.fn.timer_start(60, function()
-            update()
-        end)
-    end
-
-    --- Start the spinner
-    -- @return nil
-    local function start()
-        if not state.is_running then
-            update()
-
-            state.is_running = true
-        end
-    end
-
-    --- Stops the timer
-    -- @return nil
-    local function stop()
-        vim.fn.timer_stopall()
-
-        state.is_ready = false
-        state.is_running = false
-    end
-
-    local instance = vim.tbl_deep_extend("force", state, {
-        get_message = get_message,
-        update = update,
-        start = start,
-        stop = stop,
-    })
-
-    table.insert(M.instances, instance)
+    table.insert(M.queue, instance)
 
     return instance.id
 end
 
---- Start the instance by given id
+--- Start the instance by given id by marking it as ready to run
 -- @param id: string - id of the instance to start
 -- @return nil
 M.start = function(id)
-    for _, instance in pairs(M.instances) do
+    for _, instance in ipairs(M.queue) do
         if instance.id == id then
             instance.is_ready = true
-
-            return
         end
     end
 end
 
 --- Stop the instance by given id by removing it from the list
--- @param id: string - id of the instance to stop
+-- @param id: string - id of the instance to stop and remove
 -- @return nil
 M.stop = function(id)
-    local instances = {}
+    local filtered_list = {}
 
-    for _, instance in pairs(M.instances) do
+    for _, instance in ipairs(M.queue) do
         if instance.id ~= id then
-            table.insert(instances, instance)
-        else
-            instance.stop()
+            table.insert(filtered_list, instance)
         end
     end
 
-    M.instances = instances
+    M.queue = filtered_list
 end
 
---- Get the first instance message if there are instances
+--- Update the spinner instance recursively
+-- @return nil
+M.update_spinner = function()
+    M.state.current_spinner = SPINNERS[M.state.index]
+
+    M.state.index = M.state.index + 1
+
+    if M.state.index == 10 then
+        M.state.index = 1
+    end
+
+    vim.fn.timer_start(60, function()
+        M.update_spinner()
+    end)
+end
+
+--- Get the first ready instance message if there are instances
 -- @return string
 M.get = function()
-    if vim.tbl_isempty(M.instances) then
-        return ""
-    else
-        local instance = M.instances[1]
+    local active_instance = nil
 
-        if instance.is_ready and not instance.is_running then
-            instance.start()
+    for _, instance in pairs(M.queue) do
+        if not active_instance and instance.is_ready then
+            active_instance = instance
         end
-
-        return instance.get_message()
     end
+
+    if not active_instance then
+        -- FIXME: this is killing all timers, so if a user has any timers, it will interrupt them
+        -- like lsp status
+        vim.fn.timer_stopall()
+
+        M.state.is_running = false
+        M.state.current_spinner = ""
+        M.state.index = 1
+
+        return ""
+    end
+
+    if active_instance and not M.state.is_running then
+        M.state.is_running = true
+
+        M.update_spinner()
+    end
+
+    return M.state.current_spinner .. " " .. active_instance.message
 end
 
 return M
