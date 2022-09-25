9e81a0514489479c2e4bf31efc94a227dcc96ed1
René Heinrich <h.rene.84@gmail.com>
2022-09-26 00:26:27 +0200

    feat(clean version): clean ~ from version as well (#133)
    Co-authored-by: René Heinrich <renes.postbox@gmail.com>

--- lua/package-info/helpers/clean_version.lua
+++ lua/package-info/helpers/clean_version.lua
@@ -1,10 +1,10 @@
---- Strips ^ from version
--- @param value: string - value from which to strip ^ from
+--- Strips ^ and ~ from version
+-- @param value: string - value from which to strip ^ and ~ from
 -- @return string?
 return function(value)
     if value == nil then
         return nil
     end
 
-    return value:gsub("%^", "")
+    return value:gsub("%^", ""):gsub("~", "")
 end
