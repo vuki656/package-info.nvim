c72896ace0fcd0faca9f49b40d8c967ba133c5b1
Maksim Karelov <max.karelov@gmail.com>
2023-03-21 18:34:25 +0600

    Fix stylua errors

--- lua/telescope/_extensions/package_info.lua
+++ lua/telescope/_extensions/package_info.lua
@@ -1,6 +1,6 @@
 local has_telescope, telescope = pcall(require, "telescope")
 if not has_telescope then
-  error("This extension requires 'telescope.nvim'. (https://github.com/nvim-telescope/telescope.nvim)")
+    error("This extension requires 'telescope.nvim'. (https://github.com/nvim-telescope/telescope.nvim)")
 end
 
 local telescope_package_info_config = require("telescope._extensions.package_info.config")
--- lua/telescope/_extensions/package_info/picker.lua
+++ lua/telescope/_extensions/package_info/picker.lua
@@ -11,72 +11,74 @@ local M = {}
 function M.picker()
     local opts = telescope_package_info_config.opts or {}
 
-    pickers.new(opts, {
-        prompt_title = "Package info",
-        finder = finders.new_table({
-            results = {
-                {
-                    "Show latest versions",
+    pickers
+        .new(opts, {
+            prompt_title = "Package info",
+            finder = finders.new_table({
+                results = {
                     {
-                        command = "show",
-                        args = {},
+                        "Show latest versions",
+                        {
+                            command = "show",
+                            args = {},
+                        },
+                    },
+                    {
+                        "Show latest versions (force)",
+                        {
+                            command = "show",
+                            args = { force = true },
+                        },
+                    },
+                    {
+                        "Hide latest versions",
+                        {
+                            command = "hide",
+                            args = {},
+                        },
+                    },
+                    {
+                        "Delete package",
+                        {
+                            command = "delete",
+                            args = {},
+                        },
+                    },
+                    {
+                        "Change package version",
+                        {
+                            command = "change_version",
+                            args = {},
+                        },
+                    },
+                    {
+                        "Install package",
+                        {
+                            command = "install",
+                            args = {},
+                        },
                     },
                 },
-                {
-                    "Show latest versions (force)",
-                    {
-                        command = "show",
-                        args = { force = true },
-                    },
-                },
-                {
-                    "Hide latest versions",
-                    {
-                        command = "hide",
-                        args = {},
-                    },
-                },
-                {
-                    "Delete package",
-                    {
-                        command = "delete",
-                        args = {},
-                    },
-                },
-                {
-                    "Change package version",
-                    {
-                        command = "change_version",
-                        args = {},
-                    },
-                },
-                {
-                    "Install package",
-                    {
-                        command = "install",
-                        args = {},
-                    },
-                },
-            },
-            entry_maker = function(entry)
-                return {
-                    value = entry[2],
-                    display = entry[1],
-                    ordinal = entry[1],
-                }
+                entry_maker = function(entry)
+                    return {
+                        value = entry[2],
+                        display = entry[1],
+                        ordinal = entry[1],
+                    }
+                end,
+            }),
+            sorter = conf.generic_sorter(opts),
+            attach_mappings = function(prompt_bufnr)
+                actions.select_default:replace(function()
+                    actions.close(prompt_bufnr)
+                    local selection = action_state.get_selected_entry()
+                    local value = selection.value
+                    require("package-info")[value.command](value.args)
+                end)
+                return true
             end,
-        }),
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                local selection = action_state.get_selected_entry()
-                local value = selection.value
-                require("package-info")[value.command](value.args)
-            end)
-            return true
-        end,
-    }):find()
+        })
+        :find()
 end
 
 return M
