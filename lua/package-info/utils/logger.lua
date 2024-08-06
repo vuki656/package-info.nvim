a266064173647caf51ccdc3ad697146ae2c7577f
agoodshort <adrien.goodshort@gmail.com>
2024-08-06 23:09:15 +0100

    feat: add nvim-notify support

--- lua/package-info/utils/logger.lua
+++ lua/package-info/utils/logger.lua
@@ -5,7 +5,33 @@ local M = {}
 -- @param highlight_group: string - highlight group to use when printing the message
 -- @return nil
 M.__print = function(message, highlight_group)
-    vim.api.nvim_echo({ { "PackageInfo: " .. message, highlight_group or "" } }, {}, {})
+    if pcall(require, "notify") then
+        if not highlight_group then
+            highlight_group = "InfoMsg"
+        end
+
+        local level = {
+            ["InfoMsg"] = {
+                log_level = vim.log.levels.INFO,
+                log_symbol = "󰗠 ",
+            },
+            ["ErrorMsg"] = {
+                log_level = vim.log.levels.ERROR,
+                log_symbol = "󰅙 ",
+            },
+            ["WarningMsg"] = {
+                log_level = vim.log.levels.WARN,
+                log_symbol = " ",
+            },
+        }
+        vim.notify(message, level[highlight_group].log_level, {
+            title = "package-info.nvim",
+            icon = level[highlight_group].log_symbol,
+            timeout = 3000,
+        })
+    else
+        vim.api.nvim_echo({ { "PackageInfo: " .. message, highlight_group or "" } }, true, {})
+    end
 end
 
 --- Prints an error message
