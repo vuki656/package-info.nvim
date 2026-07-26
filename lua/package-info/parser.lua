88c5d25f7e8d8f38c427d8234fbcb560ffe4aa48
Domagoj Vukovic <off.vukovic@gmail.com>
2026-07-26 20:13:30 +0200

    fix: handle null and non-object values when parsing package.json

--- lua/package-info/parser.lua
+++ lua/package-info/parser.lua
@@ -28,34 +28,48 @@ local function intersect(t1, t2)
     return intersectKeySets(s1, s2)
 end
 
+local function as_table(value)
+    if type(value) ~= "table" then
+        return {}
+    end
+
+    return value
+end
+
 M.parse_buffer = function()
     local buffer_lines = vim.api.nvim_buf_get_lines(state.buffer.id, 0, -1, false)
-    local buffer_json_value = vim.json.decode(table.concat(buffer_lines))
+    local buffer_json_value = as_table(vim.json.decode(table.concat(buffer_lines)))
 
-    local intersection = intersect(buffer_json_value["devDependencies"] or {}, buffer_json_value["dependencies"] or {})
-    local all_dependencies_json =
-        vim.tbl_extend("force", {}, buffer_json_value["devDependencies"] or {}, buffer_json_value["dependencies"] or {})
+    local dev_dependencies = as_table(buffer_json_value["devDependencies"])
+    local dependencies = as_table(buffer_json_value["dependencies"])
+
+    local intersection = intersect(dev_dependencies, dependencies)
+    local all_dependencies_json = vim.tbl_extend("force", {}, dev_dependencies, dependencies)
 
     local installed_dependencies = {}
     local errored_dependencies = {}
 
     for name, version in pairs(all_dependencies_json) do
+        local has_version_string = type(version) == "string"
+
         installed_dependencies[name] = {
-            current = clean_version(version),
+            current = has_version_string and clean_version(version) or nil,
         }
         if intersection[name] ~= nil then
             errored_dependencies[name] = {
                 diagnostic = "DUPLICATED",
             }
-        elseif not is_valid_version_syntax(version) then
+        elseif not has_version_string or not is_valid_version_syntax(version) then
             errored_dependencies[name] = {
                 diagnostic = "INVALID VERSION",
             }
         end
     end
 
+    local package_name = buffer_json_value["name"]
+
     state.buffer.lines = buffer_lines
-    state.buffer.package_name = buffer_json_value["name"]
+    state.buffer.package_name = type(package_name) == "string" and package_name or nil
     state.dependencies.installed = installed_dependencies
     state.dependencies.invalid = errored_dependencies
 end
--- lua/package-info/tests/suites/parser/parse_buffer_spec.lua
+++ lua/package-info/tests/suites/parser/parse_buffer_spec.lua
@@ -61,4 +61,105 @@ T["should flag dependencies with a templated version as invalid"] = function()
     expect.equality(invalid.next.diagnostic, "INVALID VERSION")
 end
 
+T["should treat a null dependency block as empty"] = function()
+    local package_json = file.create({
+        name = "package.json",
+        randomize = true,
+        go = true,
+        content = [[
+                {
+                    "name": "repo-name",
+                    "dependencies": null,
+                    "devDependencies": null
+                }
+            ]],
+    })
+
+    core.load_plugin()
+
+    expect.no_error(parser.parse_buffer)
+
+    local installed = state.dependencies.installed
+
+    file.delete(package_json.path)
+
+    expect.equality(installed, {})
+end
+
+T["should flag a dependency with a null version as invalid"] = function()
+    local package_json = file.create({
+        name = "package.json",
+        randomize = true,
+        go = true,
+        content = [[
+                {
+                    "name": "repo-name",
+                    "dependencies": {
+                        "react": null,
+                        "next": "12.0.3"
+                    }
+                }
+            ]],
+    })
+
+    core.load_plugin()
+
+    expect.no_error(parser.parse_buffer)
+
+    local installed = state.dependencies.installed
+    local invalid = state.dependencies.invalid
+
+    file.delete(package_json.path)
+
+    expect.equality(installed.react.current, nil)
+    expect.equality(invalid.react.diagnostic, "INVALID VERSION")
+    expect.equality(invalid.next, nil)
+end
+
+T["should not crash on a package.json that is not an object"] = function()
+    local package_json = file.create({
+        name = "package.json",
+        randomize = true,
+        go = true,
+        content = "12",
+    })
+
+    core.load_plugin()
+
+    expect.no_error(parser.parse_buffer)
+
+    local installed = state.dependencies.installed
+    local package_name = state.buffer.package_name
+
+    file.delete(package_json.path)
+
+    expect.equality(installed, {})
+    expect.equality(package_name, nil)
+end
+
+T["should ignore a null package name"] = function()
+    local package_json = file.create({
+        name = "package.json",
+        randomize = true,
+        go = true,
+        content = [[
+                {
+                    "name": null,
+                    "dependencies": {
+                        "react": "16.0.0"
+                    }
+                }
+            ]],
+    })
+
+    core.load_plugin()
+    parser.parse_buffer()
+
+    local package_name = state.buffer.package_name
+
+    file.delete(package_json.path)
+
+    expect.equality(package_name, nil)
+end
+
 return T
